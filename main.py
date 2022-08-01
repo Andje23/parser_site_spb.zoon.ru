e1dff1d7a06c44c9f413463c2283890fa0d2d043
Andje23 <andje23@gmail.com>
2022-08-01 22:53:04 +0300

    Added a list of libraries required for further work

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+import re
+from urllib.parse import unquote
+import random
+import json
+
