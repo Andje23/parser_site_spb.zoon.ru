2b87b51adb57b169c92c34173579fe90729375e5
Andje23 <andje23@gmail.com>
2022-08-01 22:55:50 +0300

    Added headers for further reference to the site

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ from urllib.parse import unquote
 import random
 import json
 
+headers = {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36"
+}
