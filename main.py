5cfb8eb116d3adfbe1ced65248c3f7a40c594314
Andje23 <andje23@gmail.com>
2022-08-17 18:36:29 +0300

    implementation writing to file in function get_data

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+import json
+import random
+import re
+import time
+from urllib.parse import unquote
+
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
-import time
-import re
-from urllib.parse import unquote
-import random
-import json
 
 headers = {
     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
@@ -48,14 +49,13 @@ def get_source_html(url: str) -> None:
         driver.close()
         driver.quit()
 
-
     def get_items_urls(file_path: str) -> str:
         with open(file_path) as file:
             scr = file.read()
-            
+
         soup = BeautifulSoup(scr, "lxml")
         items_divs = soup.find_all("div", class_="service-description")
-        
+
         urls = []
         for item in items_divs:
             item_url = item.find("div", class_="H3").find("a").get("href")
@@ -103,7 +103,7 @@ def get_data(file_path: str) -> str:
             item_site = soup.find(text=re.compile("Сайт|официальный сайт")).find_next().text.strip()
         except Exception as _ex:
             item_site = None
-        
+
         social_networks_list = []
         try:
             item_social_networks = soup.find(text=re.compile("Страница в соцсетях")).find_next().find_all("a")
@@ -127,16 +127,22 @@ def get_data(file_path: str) -> str:
 
         time.sleep(random.randrange(2, 5))
 
-        if count%10 == 0:
+        if count % 10 == 0:
             time.sleep(random.randrange(5, 9))
 
+        print(f"[+] Processed: {count}/{url_count}")
 
+        count += 1
 
+    with open("parse/result.json", "w") as file:
+        json.dump(result_list, file, indent=4, ensure_ascii=False)
 
+    return "[INFO] Data collected successfully!"
 
 
 def main():
-    get_source_html(url="https://spb.zoon.ru/medical/?search_query_form=1&m%5B5200e522a0f302f066000055%5D=1&center%5B%5D=59.91878264665887&center%5B%5D=30.342586983263384&zoom=10")
+    get_source_html(
+        url="https://spb.zoon.ru/medical/?search_query_form=1&m%5B5200e522a0f302f066000055%5D=1&center%5B%5D=59.91878264665887&center%5B%5D=30.342586983263384&zoom=10")
 
 
 if __name__ == '__main__':
